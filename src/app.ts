9df0ce4c64831f9178b1c6ffa6ddd2a86d84b81b
Elvis <elvis.adomnica@gmail.com>
2023-08-17 21:11:23 +0200

    Clean up code

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,11 @@
 import { LitElement, css, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { customElement } from "lit/decorators.js";
+import "./lib/pg-widget";
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
 @customElement("app-root")
 export class App extends LitElement {
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({ type: Number })
-  count = 0;
-
   render() {
-    return html`
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-    `;
-  }
-
-  private _onClick() {
-    this.count++;
+    return html`<pg-widget />`;
   }
 
   static styles = css`
@@ -36,34 +14,6 @@ export class App extends LitElement {
       margin: 0 auto;
       padding: 2rem;
       text-align: center;
-    }
-
-    .card {
-      padding: 2em;
-    }
-
-    ::slotted(h1) {
-      font-size: 3.2em;
-      line-height: 1.1;
-    }
-
-    button {
-      border-radius: 8px;
-      border: 1px solid transparent;
-      padding: 0.6em 1.2em;
-      font-size: 1em;
-      font-weight: 500;
-      font-family: inherit;
-      background-color: #1a1a1a;
-      cursor: pointer;
-      transition: border-color 0.25s;
-    }
-    button:hover {
-      border-color: #646cff;
-    }
-    button:focus,
-    button:focus-visible {
-      outline: 4px auto -webkit-focus-ring-color;
     }
 
     @media (prefers-color-scheme: light) {
