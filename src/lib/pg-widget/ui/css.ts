a48b30bc0c02464417ce445b7f21ef640fa7a809
Elvis <elvis.adomnica@gmail.com>
2023-08-19 13:00:15 +0200

    Add gray colors for text and background

--- src/lib/pg-widget/ui/css.ts
+++ src/lib/pg-widget/ui/css.ts
@@ -2,7 +2,15 @@ import { css } from "lit";
 
 export const variables = css`
   :host {
-    --bg-opacity: 1;
+    --color-gray-100: rgba(247, 250, 252, 1);
+    --color-gray-200: rgba(237, 242, 247, 1);
+    --color-gray-300: rgba(226, 232, 240, 1);
+    --color-gray-400: rgba(203, 213, 224, 1);
+    --color-gray-500: rgba(160, 174, 192, 1);
+    --color-gray-600: rgba(113, 128, 150, 1);
+    --color-gray-700: rgba(74, 85, 104, 1);
+    --color-gray-800: rgba(45, 55, 72, 1);
+    --color-gray-900: rgba(26, 32, 44, 1);
   }
 `;
 
@@ -26,12 +34,108 @@ export const flex1 = css`
 
 export const bgGray100 = css`
   .bg-gray-100 {
-    background-color: rgba(247, 250, 252, var(--bg-opacity));
+    background-color: var(--color-gray-100);
+  }
+`;
+
+export const bgGray200 = css`
+  .bg-gray-200 {
+    background-color: var(--color-gray-200);
+  }
+`;
+
+export const bgGray300 = css`
+  .bg-gray-300 {
+    background-color: var(--color-gray-300);
+  }
+`;
+
+export const bgGray400 = css`
+  .bg-gray-400 {
+    background-color: var(--color-gray-400);
+  }
+`;
+
+export const bgGray500 = css`
+  .bg-gray-500 {
+    background-color: var(--color-gray-500);
+  }
+`;
+
+export const bgGray600 = css`
+  .bg-gray-600 {
+    background-color: var(--color-gray-600);
+  }
+`;
+
+export const bgGray700 = css`
+  .bg-gray-700 {
+    background-color: var(--color-gray-700);
+  }
+`;
+
+export const bgGray800 = css`
+  .bg-gray-800 {
+    background-color: var(--color-gray-800);
   }
 `;
 
 export const bgGray900 = css`
   .bg-gray-900 {
-    background-color: rgba(26, 32, 44, var(--bg-opacity));
+    background-color: var(--color-gray-900);
+  }
+`;
+
+export const textGray100 = css`
+  .text-gray-100 {
+    color: var(--color-gray-100);
+  }
+`;
+
+export const textGray200 = css`
+  .text-gray-200 {
+    color: var(--color-gray-200);
+  }
+`;
+
+export const textGray300 = css`
+  .text-gray-300 {
+    color: var(--color-gray-300);
+  }
+`;
+
+export const textGray400 = css`
+  .text-gray-400 {
+    color: var(--color-gray-400);
+  }
+`;
+
+export const textGray500 = css`
+  .text-gray-500 {
+    color: var(--color-gray-500);
+  }
+`;
+
+export const textGray600 = css`
+  .text-gray-600 {
+    color: var(--color-gray-600);
+  }
+`;
+
+export const textGray700 = css`
+  .text-gray-700 {
+    color: var(--color-gray-700);
+  }
+`;
+
+export const textGray800 = css`
+  .text-gray-800 {
+    color: var(--color-gray-800);
+  }
+`;
+
+export const textGray900 = css`
+  .text-gray-900 {
+    color: var(--color-gray-900);
   }
 `;
